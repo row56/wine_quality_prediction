04020589fbc5c4b94f83d05b71165239b517ea33
vipin2310 <72501790+vipin2310@users.noreply.github.com>
2024-01-04 17:21:26 +0100

    Implemented PPR

--- src/pursuit_projection.R
+++ src/pursuit_projection.R
@@ -3,4 +3,94 @@
 # Perform predictions
 
 # ---- Load data and libraries from Setup.R file -------------------------------
-source("Setup.R")
+source("src/setup.R")
+
+# ---- Import libraries --------------------------------------------------------
+
+# ---- Perform pursuit projection ----------------------------------------------
+
+formula <- quality ~ .
+
+ppr.obj = ppr(formula, data = train, nterms = 10)
+
+summary(ppr.obj)
+plot(ppr.obj)
+
+# ---- Perform predictions -----------------------------------------------------
+
+# Predict the quality of the test set
+pred <- predict(ppr.obj, newdata = test)
+
+# Calculate the MSE
+mse <- mean((pred - test$quality)^2)
+
+# Print the MSE
+print(mse)
+
+# ---- Tune the number of terms ------------------------------------------------
+
+max_terms <- 15
+
+# Create a vector of MSEs
+mse <- rep(NA, max_terms)
+
+# Create training and validation sets
+train_size <- floor(0.8 * nrow(train))
+train_indices <- sample(seq_len(nrow(train)), size = train_size)
+
+tuning_train <- train[train_indices, ]
+tuning_validation <- train[-train_indices, ]
+
+# Loop over the number of terms
+for (i in 1:max_terms) {
+  # Fit the model
+  ppr.obj <- ppr(formula, data = tuning_train, nterms = i)
+
+  # Predict on the validation set
+  val.ppr <- predict(ppr.obj, newdata = tuning_validation)
+
+  # Compute the validation MSE
+  mse[i] <- mean((tuning_validation$quality - val.ppr)^2)
+}
+
+# Plot the MSEs
+plot(1:max_terms, mse, type = "b", xlab = "Number of terms", ylab = "MSE")
+
+# Highlight the minimum MSE
+points(which.min(mse), mse[which.min(mse)], col = "red", pch = 19)
+
+best_mse <- mse[which.min(mse)]
+best_nterms <- which.min(mse)
+
+# Print the minimum MSE
+print(paste("Minimum MSE with nterms = ", best_nterms, ": ", best_mse))
+
+# ---- Final model -------------------------------------------------------------
+
+# Fit the model with the best number of terms
+ppr.obj <- ppr(formula, data = train, nterms = best_nterms)
+
+# Predict on the test set
+predicted_values <- predict(ppr.obj, newdata = test)
+
+actual_values <- test$quality
+
+# Create a scatter plot of the actual values vs the predicted values
+plot(actual_values, predicted_values, xlab = "Actual Values",
+     ylab = "Predicted Values", main = "Actual vs Predicted Values",
+     xlim = c(3, 8), ylim = c(3, 8))
+
+# Add a line showing where the predicted values equal the actual values
+abline(a = 0, b = 1, col = "red")
+
+# Create residiual plot
+residuals <- actual_values - predicted_values
+plot(predicted_values, residuals)
+abline(h = 0)  # Adds a horizontal line at 0 for reference
+
+# Maybe: The model is predicting in range between 5 and 6 often times
+# for the quality, probably since more than 80% of are data are 5 or 6.
+# -> Overfitting to middle area
+
+# ---- Clean up ----------------------------------------------------------------
+rm(list = ls())
