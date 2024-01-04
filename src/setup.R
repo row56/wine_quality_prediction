13cdd263bcbd42ee6f92bb7f6195276288358a51
vipin2310 <72501790+vipin2310@users.noreply.github.com>
2024-01-04 15:09:11 +0100

    Data loaded and splitted

--- src/setup.R
+++ src/setup.R
@@ -1,6 +1,17 @@
-# Load packages needed for the project
+# Set seed for reproducibility
+set.seed(123)
+
 # Load the data
-# Process the data
-# (e. g. split the data already)
+wine_data <- read.csv("data/winequality-red.csv", sep = ";")
+
+# Split the data
+train_size <- floor(0.8 * nrow(wine_data))
+train_indices <- sample(seq_len(nrow(wine_data)), size = train_size)
+
+train <- wine_data[train_indices, ]
+test <- wine_data[-train_indices, ]
 
 # Also clean the environment variables not needed further
+keep_vars <- c("train", "test", "wine_data")
+
+rm(list = setdiff(ls(), keep_vars))
--- src/spline_method.R
+++ src/spline_method.R
@@ -4,3 +4,10 @@
 
 # ---- Load data and libraries from Setup.R file -------------------------------
 source("Setup.R")
+
+
+# Comment from Downie:
+# The spline smoothing uses just one predictor variable.
+# I would like you to investigate at least 3 spline models using each time a
+# different variable. Perhaps it makes sense to choose variables you think (in
+# advance) will have the biggest non-linear effect on wine quality.
