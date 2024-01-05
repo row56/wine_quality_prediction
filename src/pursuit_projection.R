5140a16dadabf15bb5a5b288dda15da1335eed81
vipin2310 <72501790+vipin2310@users.noreply.github.com>
2024-01-05 15:47:22 +0100

    PPR simple and weighted added + Functions to easy streamline

--- src/pursuit_projection.R
+++ src/pursuit_projection.R
@@ -1,11 +1,11 @@
-# Setup for pursuit projection
-# Perform and visualize the training
-# Perform predictions
-
 # ---- Load data and libraries from Setup.R file -------------------------------
+
 source("src/setup.R")
+clean_up <- FALSE
 
 # ---- Import libraries --------------------------------------------------------
+
+# ---- Define functions --------------------------------------------------------
 
 build_weights <- function(data) {
     # Create a matrix of weights
@@ -24,69 +24,118 @@ build_weights <- function(data) {
     return(weights)
 }
 
-# ---- Perform pursuit projection ----------------------------------------------
+evaluate_model <- function(model, data) {
+    # Predict on the data
+    predicted_values <- predict(model, newdata = data)
+
+    actual_values <- data$quality
+
+    # Create a scatter plot of the actual values vs the predicted values
+    plot(actual_values, predicted_values, xlab = "Actual Values",
+        ylab = "Predicted Values", main = "Actual vs Predicted Values",
+        xlim = c(3, 8), ylim = c(3, 8))
+
+    # Add a line showing where the predicted values equal the actual values
+    abline(a = 0, b = 1, col = "red")
+
+    # Compute the MSE
+    mse <- mean((actual_values - predicted_values)^2)
+
+    # Print the MSE
+    print(paste("Test MSE: ", mse))
+
+    # Return the Predicted Values and MSE
+    return(list(mse = mse, predicted_values = predicted_values))
+}
+
+tune_nterms <- function(formula, train_data, val_data, max_terms = 15,
+    weights = NULL) {
+
+    # Set the weights to 1 if not provided
+    if (is.null(weights)) {
+        weights <- as.vector(rep(1, nrow(train_data)))
+    }
+
+    # Store the weights in the environment so ppr() can access them
+    weights_in_env <- NULL
+    weights_in_env <<- weights
+
+    # Create a vector of MSEs
+    mse <- rep(0, max_terms)
+
+    # Loop over the number of terms
+    for (i in 1:max_terms) {
+
+        # Fit the model
+        ppr_obj <- ppr(formula, train_data, weights = weights_in_env,
+            nterms = i)
+
+        # Predict on the validation set
+        val_ppr <- predict(ppr_obj, newdata = val_data)
+
+        # Compute the validation MSE
+        mse[i] <- mean((val_data$quality - val_ppr)^2)
+    }
+
+    if (exists("weights_in_env", envir = .GlobalEnv)) {
+        rm(weights_in_env, pos = ".GlobalEnv")
+    }
+
+    # Plot the MSEs
+    plot(1:max_terms, mse, type = "b", xlab = "Number of terms", ylab = "MSE")
+
+    # Highlight the minimum MSE
+    points(which.min(mse), mse[which.min(mse)], col = "red", pch = 19)
+
+    best_mse <- mse[which.min(mse)]
+    best_nterms <- which.min(mse)
+
+    # Print the minimum MSE
+    print(paste("Minimum MSE with nterms = ", best_nterms, ": ", best_mse))
+
+    # Return the best MSE and number of terms
+    return(list(best_mse = best_mse, best_nterms = best_nterms, all_mse = mse))
+}
+
+# ---- Simple pursuit projection -----------------------------------------------
 
 formula <- quality ~ .
 
-# Tune number of terms
-
-max_terms <- 15
-
-# Create a vector of MSEs
-mse <- rep(NA, max_terms)
-
-weights <- build_weights(train)
-
-# Loop over the number of terms
-for (i in 1:max_terms) {
-    # Fit the model
-    ppr.obj <- ppr(formula, data = train, nterms = i, weights = weights)
-
-    # Predict on the validation set
-    val.ppr <- predict(ppr.obj, newdata = validation)
-
-    # Compute the validation MSE
-    mse[i] <- mean((validation$quality - val.ppr)^2)
-}
-
-# Plot the MSEs
-plot(1:max_terms, mse, type = "b", xlab = "Number of terms", ylab = "MSE")
-
-# Highlight the minimum MSE
-points(which.min(mse), mse[which.min(mse)], col = "red", pch = 19)
-
-best_mse <- mse[which.min(mse)]
-best_nterms <- which.min(mse)
-
-# Print the minimum MSE
-print(paste("Minimum MSE with nterms = ", best_nterms, ": ", best_mse))
-
-# ---- Test final model --------------------------------------------------------
+tuning_result <- tune_nterms(formula, train, validation)
 
 # Fit the model with the best number of terms
-ppr.obj <- ppr(formula, data = train, nterms = best_nterms)
+ppr_simple <- ppr(formula, data = train, nterms = tuning_result$best_nterms)
 
-# Predict on the test set
-predicted_values <- predict(ppr.obj, newdata = test)
-
-actual_values <- test$quality
-
-# Create a scatter plot of the actual values vs the predicted values
-plot(actual_values, predicted_values, xlab = "Actual Values",
-     ylab = "Predicted Values", main = "Actual vs Predicted Values",
-     xlim = c(3, 8), ylim = c(3, 8))
-
-# Add a line showing where the predicted values equal the actual values
-abline(a = 0, b = 1, col = "red")
-
-# Create residiual plot
-residuals <- actual_values - predicted_values
-plot(predicted_values, residuals)
-abline(h = 0)  # Adds a horizontal line at 0 for reference
+# Evaluate the model on the test set
+test_results <- evaluate_model(ppr.obj, test)
 
 # Maybe: The model is predicting in range between 5 and 6 often times
 # for the quality, probably since more than 80% of are data are 5 or 6.
 # -> Overfitting to middle area
 
+# ---- Pursuit projection regression with weights ------------------------------
+
+# Formula and weights
+formula <- quality ~ .
+
+# Tune number of terms
+tuning_result <- tune_nterms(formula, train, validation,
+    weights = build_weights(train))
+
+# Fit the model with the best number of terms
+ppr_weighted <- ppr(formula, data = train, nterms = tuning_result$best_nterms)
+
+# Evaluate the model on the test set
+test_results <- evaluate_model(ppr.obj, test)
+
+# Higher MSE, but the model is a little bit better in predicting the minority
+# classes
+
+# ---- Pursuit projection regression with stratified sampling ------------------
+
+# TODO
+
 # ---- Clean up ----------------------------------------------------------------
-# rm(list = ls())
+if (clean_up) {
+    rm(list = ls())
+}
