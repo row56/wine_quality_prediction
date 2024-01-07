075ec535dc33527b386e4cb7aa3760322f4ffb30
robert-wienroeder <kontakt@robert-wienroeder.de>
2024-01-07 15:37:11 +0100

    fehlerbehebung hpo spline

--- src/spline_method2.R
+++ src/spline_method2.R
@@ -15,12 +15,12 @@ library(yardstick) # For huber loss
 predictor <- "alcohol"
 target <- "quality"
 
+mixed_sampled_train <- balance_classes_mixed_sampling(train, 200)
+mixed_sampled_train <- mixed_sampled_train[c(predictor, target)]
+
 train <- train[c(predictor, target)]
 validation <- validation[c(predictor, target)]
 test <- test[c(predictor, target)]
-
-# Reset index
-rownames(train) <- NULL
 
 
 # ---- Plot Predictor vs Target to see relationship ------------------------------
@@ -33,7 +33,6 @@ plot <- create_violin_plot(
             xlab = target,
             ylab = predictor)
 print(plot)
-
 
 
 # -------- HPO on degrees of freedom by Huber loss -------------------------------
@@ -55,7 +54,7 @@ tune_spline_df <- function(train_data, val_data, predictor, weights = NULL, titl
             Subset the data to one predictor and quality column.")
     }
 
-    df_values = c(2:length(unique(train$alcohol)))
+    df_values <- c(2:length(unique(train$alcohol)))
 
     # Create a vector for Huber losses
     huber <- rep(0, length(df_values))
@@ -89,7 +88,7 @@ tune_spline_df <- function(train_data, val_data, predictor, weights = NULL, titl
     }
 
     # Plot the Huber losses
-    plot(1:length(df_values), huber, type = "b", xlab = "df",
+    plot(seq_along(df_values), huber, type = "b", xlab = "df",
         ylab = "Huber Loss", main = paste("Huber Loss vs df (",
         title_suffix, ")"))
 
@@ -107,28 +106,15 @@ tune_spline_df <- function(train_data, val_data, predictor, weights = NULL, titl
         all_huber = huber))
 }
 
-
-
-
-
-
-
-# ---- Import libraries --------------------------------------------------------
-
-
-
 # ---- Perform Spline smoothing simple only with HPO ---------------------------
 
 tuning_result <- tune_spline_df(train, validation, predictor,
     title_suffix = "Simple")
 
-
-print(tuning_result$best_df)
-
 # Fit the model with the best number of df
 spline_simple <- smooth.spline(train[[predictor]], train[[target]], df=tuning_result$best_df)
 
-test_results_simple <- evaluate_model(spline_simple, test,
+test_result_simple <- evaluate_model(spline_simple, test,
     title = "Simple spline with HPO on df")
 
 
@@ -136,30 +122,36 @@ test_results_simple <- evaluate_model(spline_simple, test,
 
 weights <- build_weights(train)
 
+tuning_result <- tune_spline_df(train, validation, predictor, weights,
+    title_suffix = "Weighted")
+
 spline_weighted <- smooth.spline(train[[predictor]], train[[target]], df=tuning_result$best_df, w = weights)
 
 
-test_results_weighted <- evaluate_model(spline_weighted, test,
+test_result_weighted <- evaluate_model(spline_weighted, test,
     title = "Weighted spline with HPO on df")
 
 # ---- Perform Spline smoothing with mixed sampling ----------------------------
 
-mixed_sampled_train <- balance_classes_mixed_sampling(train, 200)
+tuning_result <- tune_spline_df(mixed_sampled_train, validation, predictor,
+    title_suffix = "Mixed sampled")
 
 spline_mixed_sampling <- smooth.spline(mixed_sampled_train[[predictor]], mixed_sampled_train[[target]], df=tuning_result$best_df)
 
-test_results_mixed <- evaluate_model(spline_mixed_sampling, test,
+test_result_mixed <- evaluate_model(spline_mixed_sampling, test,
     title = "Mixed sampled spline with HPO on df")
 
 # ---- Perform Spline smoothing with mixed sampling and weights ----------------
 
-mixed_sampled_train <- balance_classes_mixed_sampling(train, 200)
 weights <- build_weights(mixed_sampled_train)
+
+tuning_result <- tune_spline_df(mixed_sampled_train, validation, predictor, weights,
+    title_suffix = "Mixed sampled and weighted")
 
 spline_mixed_weighted <- smooth.spline(mixed_sampled_train[[predictor]], mixed_sampled_train[[target]], df=tuning_result$best_df, w = weights)
 
 
-test_results_mixed_weighted <- evaluate_model(spline_mixed_weighted, test,
+test_result_mixed_weighted <- evaluate_model(spline_mixed_weighted, test,
     title = "Mixed sampled and weighted spline with HPO on df")
 
 # ---- Create a table with the results -----------------------------------------
